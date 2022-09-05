fd73556aaa1c853ee4c388bd2fdc502a7777b4cf
CHJ-X <1693189780@qq.com>
2022-09-05 12:30:41 +0800

    创建项目

---
+++ src/main/java/com/chj/jx_mall/JxMallApplication.java
@@ -0,0 +1,13 @@
+package com.chj.jx_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chj/jx_mall/JxMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chj.jx_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
